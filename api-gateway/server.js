6fd945e3fec8e6d4d6ac2f6befb3f478ff7ca42a
antierisalu <antikaz@gmail.com>
2024-10-05 15:17:39 +0300

    3 VM-s with nodejs and npm are up and running.

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,9 +1,9 @@
-const http = require('http');
+import { createServer } from 'http';
 
 const hostname = '0.0.0.0'; // Listen on all interfaces
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Africa!\n');
